629b21a51e8f695b65cdbf8d111470a8252b4734
jfioravante <jfioravante@autodoc.com.br>
2020-03-13 16:17:05 -0300

    Implementando validações de entidades.

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,11 +1,12 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -21,5 +22,17 @@ namespace QuickBuy.Dominio.Entidades
 
         //Pedido deve ter pelo menos um Item de pedido ou muitos itens pedidos.
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+
+            LimparMensagensValidacao();
+
+            if (!ItensPedido.Any())
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido");
+
+            if (string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Crítica - CEP deve ser preenchido");
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/Produto.cs
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -4,12 +4,16 @@ using System.Text;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Produto
+    public class Produto : Entidade
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public Decimal Preco { get; set; }
 
+        public override void Validate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
